ee27ceef9bfa0f175e5a081c185479b6e86e3af2
0biWanKenobi <metal.rulesall@gmail.com>
2021-12-30 12:24:10 +0100

    WIP: converting common mixin to composable

--- src/modules/common.js
+++ src/modules/common.js
@@ -7,104 +7,136 @@ import Scrollbar from '@/components/scrollbar/index';
 import TocNav from '@/components/toc-nav';
 
 import EDITOR_MODE from '@/utils/constants/editor-mode';
+import { computed, nextTick, ref, watch } from 'vue';
+
+export const editorProps = {
+  height: String,
+  theme: Object,
+  mode: {
+    type: String,
+    default: EDITOR_MODE.EDITABLE,
+  },
+  autofocus: Boolean,
+  placeholder: String,
+  tocNavPositionRight: Boolean,
+  tabSize: {
+    type: Number,
+    default: 2,
+  },
+  beforePreviewChange: {
+    type: Function,
+    default: (text, next) => {
+      next(text);
+    },
+  },
+};
+
+export const editorEmits = ['blur', 'change', 'save', 'image-click'];
+
+export const shouldInheritAttrs = false;
+
+export const editorComponents = {
+  [Preview.name]: Preview,
+  [Container.name]: Container,
+  [Scrollbar.name]: Scrollbar,
+  [TocNav.name]: TocNav,
+};
+
+export const useCommonEditor = (mode) => {
+  const currentMode = ref(mode.value);
+  let data = {};
+
+  const isPreviewMode = computed(() => currentMode.value === EDITOR_MODE.PREVIEW);
+  const isEditMode = computed(() => currentMode.value === EDITOR_MODE.EDIT);
+
+  watch(
+    () => mode.value,
+    (newValue) => (currentMode.value = newValue),
+    {
+      immediate: true,
+    }
+  );
+
+  watch(
+    () => currentMode.value,
+    async (newValue) => {
+      if (newValue === EDITOR_MODE.EDITABLE && this.enableSyncScroll) {
+        await nextTick(this.previewSyncScroll);
+      }
+    }
+  );
+
+  const setFocusEnd = (focusUtils) => {
+    focusUtils.editorFocusEnd();
+    focusUtils.editorScrollToTop(9999);
+    focusUtils.previewScrollTo(9999);
+  };
+
+  // change event
+  const handleChange = (emit, text, html) => {
+    emit('change', text, html);
+  };
+
+  const handleBlur = (emit, e) => {
+    emit('blur', e);
+  };
+
+  const handlePreviewImageClick = (emit, images, currentIndex) => {
+    emit('image-click', images, currentIndex);
+  };
+
+  const save = (emit, text, html) => {
+    // emit('save', this.text, this.$refs.preview.html);
+    emit('save', text, html);
+  };
+
+  const insert = (insertUtils) => {
+    insertUtils.focus();
+
+    const currentSelectedStr = insertUtils.getCurrentSelectedStr();
+    const { selected, text } = getInsertContent(currentSelectedStr);
+
+    insertUtils.replaceSelectionText(text);
+
+    insertUtils.$nextTick(() => {
+      insertUtils.changeSelctionTo(text, selected);
+    });
+  };
+
+  const onCreated = (theme, options) => {
+    if (theme) options.use(theme);
+  };
+
+  const onMounted = async (isAutofocused, focusUtils) => {
+    if (isAutofocused) {
+      await nextTick(() => setFocusEnd(focusUtils));
+    }
+  };
+
+  return {
+    data,
+    mode,
+    currentMode,
+    isPreviewMode,
+    isEditMode,
+    setFocusEnd,
+    handleChange,
+    handleBlur,
+    handlePreviewImageClick,
+    save,
+    insert,
+    onCreated,
+    onMounted,
+  };
+};
 
 export default {
-  inheritAttrs: false,
-  components: {
-    [Preview.name]: Preview,
-    [Container.name]: Container,
-    [Scrollbar.name]: Scrollbar,
-    [TocNav.name]: TocNav,
-  },
   provide() {
     return {
       markdownEditor: this,
     };
   },
-  props: {
-    height: String,
-    theme: Object,
-    mode: {
-      type: String,
-      default: EDITOR_MODE.EDITABLE,
-    },
-    autofocus: Boolean,
-    placeholder: String,
-    tocNavPositionRight: Boolean,
-    tabSize: {
-      type: Number,
-      default: 2,
-    },
-    beforePreviewChange: {
-      type: Function,
-      default: (text, next) => {
-        next(text);
-      },
-    },
-  },
-  emits: ['blur', 'change', 'save', 'image-click'],
-  data() {
-    return {
-      currentMode: this.mode,
-      uploadConfig: {},
-    };
-  },
-  watch: {
-    mode() {
-      this.currentMode = this.mode;
-    },
-    currentMode() {
-      if (this.currentMode === EDITOR_MODE.EDITABLE && this.enableSyncScroll) {
-        this.$nextTick(this.previewSyncScroll);
-      }
-    },
-  },
   created() {
     if (this.theme) this.$options.use(this.theme);
-  },
-  computed: {
-    isPreviewMode() {
-      return this.currentMode === EDITOR_MODE.PREVIEW;
-    },
-    isEditMode() {
-      return this.currentMode === EDITOR_MODE.EDIT;
-    },
-  },
-  mounted() {
-    if (this.autofocus) {
-      this.$nextTick(this.setFocusEnd);
-    }
-  },
-  methods: {
-    setFocusEnd() {
-      this.editorFocusEnd();
-      this.editorScrollToTop(9999);
-      this.previewScrollTo(9999);
-    },
-    // change event
-    handleChange(text, html) {
-      this.$emit('change', text, html);
-    },
-    handleBlur (e) {
-      this.$emit('blur', e);
-    },
-    handlePreviewImageClick(images, currentIndex) {
-      this.$emit('image-click', images, currentIndex);
-    },
-    save() {
-      this.$emit('save', this.text, this.$refs.preview.html);
-    },
-    insert(getInsertContent) {
-      this.focus();
-
-      const currentSelectedStr = this.getCurrentSelectedStr();
-      const { selected, text } = getInsertContent(currentSelectedStr);
-
-      this.replaceSelectionText(text);
-
-      this.$nextTick(() => {
-        this.changeSelctionTo(text, selected);
-      });
-    },
   },
 };
