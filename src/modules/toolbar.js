b166ce8d2b441d6efaf5f614bdf0b5a96cdc2056
0biWanKenobi <metal.rulesall@gmail.com>
2021-12-30 12:23:50 +0100

    WIP: converting toolbar mixin to composable

--- src/modules/toolbar.js
+++ src/modules/toolbar.js
@@ -1,30 +1,35 @@
 import registerToolbar from '@/utils/toolbar';
 
+export const toolbarProps = {
+  leftToolbar: {
+    type: String,
+    default:
+      'undo redo clear | h bold italic strikethrough quote | ul ol table hr | link image code | save',
+  },
+  rightToolbar: {
+    type: String,
+    default: 'preview toc sync-scroll fullscreen',
+  },
+  toolbar: {
+    type: Object,
+    default: () => ({}),
+  },
+  disabledMenus: {
+    type: Array,
+    default: () => ['image/upload-image'],
+  },
+  toolbarConfig: {
+    type: Object,
+    default: () => ({}),
+  },
+};
+
+export const useToolbar = () => {
+  const onCreated = () => {};
+};
+
 export default function (Component) {
   return {
-    props: {
-      leftToolbar: {
-        type: String,
-        default:
-          'undo redo clear | h bold italic strikethrough quote | ul ol table hr | link image code | save',
-      },
-      rightToolbar: {
-        type: String,
-        default: 'preview toc sync-scroll fullscreen',
-      },
-      toolbar: {
-        type: Object,
-        default: () => ({}),
-      },
-      disabledMenus: {
-        type: Array,
-        default: () => ['image/upload-image'],
-      },
-      toolbarConfig: {
-        type: Object,
-        default: () => ({}),
-      },
-    },
     created() {
       const { toolbars } = Component;
 
