ba82a964db0f3e7c7aaff92f0e94afabe5c47aae
0biWanKenobi <metal.rulesall@gmail.com>
2021-12-31 22:55:02 +0100

    feat: useHotkeys composable

---
+++ src/modules/useHotkeys.ts
@@ -0,0 +1,40 @@
+import * as Hotkeys from '../hotkeys';
+import useCommon from './useCommon';
+import useEditor from './useEditor';
+
+const { isPreviewMode } = useCommon();
+const {
+  editor: { editorRegisterHotkeys },
+} = useEditor();
+
+const onMounted = () => {
+  if (isPreviewMode.value) return;
+
+  for (let hotKey of Object.values(Hotkeys)) registerHotkeys(hotKey);
+};
+
+const registerHotkeys = ({
+  modifier,
+  key,
+  action,
+  preventDefault = true,
+}: {
+  modifier?: string;
+  key: string;
+  action: Function;
+  preventDefault?: boolean;
+}) => {
+  editorRegisterHotkeys({
+    modifier,
+    key,
+    preventDefault,
+    action: (...arg) => action(this, ...arg),
+  });
+};
+
+export default () => {
+  return {
+    onMounted,
+    registerHotkeys,
+  };
+};
