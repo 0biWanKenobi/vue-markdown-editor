2d3b5b604216f7ed32ed2edf7a9ad2bfa9ed0b09
0biWanKenobi <metal.rulesall@gmail.com>
2021-12-31 22:55:13 +0100

    feat: useLang composable

---
+++ src/modules/useLang.ts
@@ -0,0 +1,31 @@
+import { computed, reactive, ref } from 'vue';
+import Lang from '@/utils/lang';
+import zhCNConfig from '@/lang/zh-CN';
+
+const lang = ref<Lang>();
+const add = (val: Record<string, any>) => {
+  lang.value.add(val);
+};
+
+const use = (name: string, val: Record<string, any>) => {
+  lang.value.use(name, val);
+};
+
+const langConfig = computed(() => {
+  const cfg = lang.value.config;
+  return cfg.langConfig[cfg.lang] ?? cfg.langConfig['zh-CN'];
+});
+
+export default () => {
+  lang.value = new Lang();
+  lang.value.config = reactive(lang.value.config);
+  lang.value.add({
+    'zh-CN': zhCNConfig,
+  });
+
+  return {
+    langConfig,
+    add,
+    use,
+  };
+};
