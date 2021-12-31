50f7a905f5de997bc9419094bcf5877e4ce8fe5b
0biWanKenobi <metal.rulesall@gmail.com>
2021-12-31 22:52:45 +0100

    refactor: IEditor
    - add DOM elements refs
    - remove unused functions
    - fix function params
    - fix function return type

--- src/interfaces/IEditor.ts
+++ src/interfaces/IEditor.ts
@@ -1,7 +1,11 @@
+import type { Ref } from 'vue';
+
 interface IEditor {
+  editorEngineEl: Ref<any>;
+  previewScrollerEl: Ref<any>;
+  previewEl: Ref<any>;
   editorFocusEnd: () => void;
-  delLineLeft: () => void;
-  getCursorLineLeftText: () => void;
+  getCursorLineLeftText: () => string;
   editorRegisterHotkeys: (...arg: any[]) => void;
   editorScrollToTop: (scrollTop: number) => void;
   getScrollInfo: () => {
@@ -14,7 +18,7 @@ interface IEditor {
   undo: () => void;
   redo: () => void;
   clear: () => void;
-  replaceSelectionText: (text: string) => void;
+  replaceSelectionText: (text: string, replacement?: string) => void;
   getCurrentSelectedStr: () => string;
   changeSelectionTo: (insertText: string, selectedText: string) => void;
 }
