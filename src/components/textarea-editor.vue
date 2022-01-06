0929476bdbd2066f2bfbf9f0cb7a6a223ae7d74b
0biWanKenobi <metal.rulesall@gmail.com>
2022-01-06 15:53:29 +0100

    refactor:  `textarea-editor` w/  Composition API
    - convert Class API to Composition API in component

--- src/components/textarea-editor.vue
+++ src/components/textarea-editor.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="v-md-textarea-editor">
     <pre :style="{ minHeight }"><section
-  v-for="(row, idx) in modelValue.split('\n')"
+  v-for="(row, idx) in modelValue?.split('\n')"
   :data-line="idx + 1"
 >{{ row || ' ' }}<br></section></pre>
     <textarea
-      ref="textarea"
+      ref="textareaEl"
       :value="modelValue"
       :placeholder="placeholder"
       spellcheck="false"
@@ -31,10 +31,10 @@
 
 <script lang="ts">
 import insertTextAtCursor from 'insert-text-at-cursor';
-import HotKeys from '@/utils/hotkeys';
-
 import { isKorean } from '@/utils/util';
-import { defineComponent } from 'vue';
+import { defineComponent, nextTick, onBeforeUnmount, onMounted, ref, toRefs, watch } from 'vue';
+import VueTypes from 'vue-types';
+import Hotkeys from '@/utils/hotkeys';
 
 export default defineComponent({
   name: 'v-md-textarea-editor',
@@ -42,158 +42,191 @@ export default defineComponent({
     modelValue: String,
     minHeight: String,
     placeholder: String,
-    historyDebounce: {
-      type: Number,
-      default: 400,
-    },
-    historyMax: {
-      type: Number,
-      default: 30,
-    },
+    historyDebounce: VueTypes.number.def(400),
+    historyMax: VueTypes.number.def(30),
   },
   emits: ['blur', 'paste', 'update:modelValue'],
-  data() {
-    return {
-      isComposing: false,
+  setup(props, { emit }) {
+    onMounted(() => {
+      saveHistory();
+
+      textareaEl.value?.$el.addEventListener('keydown', handleKeydown, false);
+    });
+
+    onBeforeUnmount(() => {
+      textareaEl.value?.$el.removeEventListener('keydown', handleKeydown, false);
+    });
+
+    const handleKeydown = (e: KeyboardEvent) => {
+      hotkeysManager.dispatch(e);
     };
-  },
-  computed: {
-    textareaEl() {
-      return this.$refs.textarea;
-    },
-  },
-  watch: {
-    modelValue() {
-      this.clearTimeout();
 
-      if (!this.triggerInputBySetHistory) {
-        this.timmer = setTimeout(() => {
-          this.saveHistory();
+    const isComposing = ref(false);
+    const textareaEl = ref();
 
-          this.clearTimeout();
-        }, this.historyDebounce);
+    const { modelValue } = toRefs(props);
+    const timer = ref<NodeJS.Timeout>();
+
+    let triggerInputBySetHistory = false;
+    watch(
+      () => modelValue.value,
+      () => {
+        clearTheTimeout();
+        if (!triggerInputBySetHistory) {
+          timer.value = setTimeout(() => {
+            saveHistory();
+
+            clearTheTimeout();
+          }, props.historyDebounce);
+        }
       }
-    },
-  },
-  created() {
-    this.historyStack = [];
-    this.historyIndex = 0;
-    this.hotkeysManager = new HotKeys();
-  },
-  mounted() {
-    this.saveHistory();
+    );
 
-    this.textareaEl.addEventListener('keydown', this.handleKeydown, false);
-  },
-  beforeUnmount() {
-    this.textareaEl.removeEventListener('keydown', this.handleKeydown, false);
-  },
-  methods: {
-    handleCompositionStart() {
-      this.isComposing = true;
-    },
-    handleCompositionUpdate(event) {
-      const text = event.target.value;
+    const handleCompositionStart = () => {
+      isComposing.value = true;
+    };
+    const handleCompositionUpdate = (event: CompositionEvent) => {
+      const text = (<HTMLInputElement>event.target)?.value;
       const lastCharacter = text[text.length - 1] || '';
-      this.isComposing = !isKorean(lastCharacter);
-    },
-    handleCompositionEnd(event) {
-      if (this.isComposing) {
-        this.isComposing = false;
-        this.handleInput(event);
+      isComposing.value = !isKorean(lastCharacter);
+    };
+
+    const handleCompositionEnd = (event: CompositionEvent) => {
+      if (isComposing.value) {
+        isComposing.value = false;
+        handleInput(event);
       }
-    },
-    handlePaste(e) {
-      this.$emit('paste', e);
-    },
-    handleBlur(e) {
-      this.$emit('blur', e);
-    },
-    registerHotkeys(...arg) {
-      this.hotkeysManager.registerHotkeys(...arg);
-    },
-    handleKeydown(e) {
-      this.hotkeysManager.dispatch(e);
-    },
-    heightAtLine(lineIndex) {
-      const el = this.$el.querySelector(`section[data-line="${lineIndex}"]`);
+    };
+
+    const handlePaste = (e: Event) => {
+      emit('paste', e);
+    };
+
+    const handleBlur = (e: Event) => {
+      emit('blur', e);
+    };
+
+    const hotkeysManager = new Hotkeys();
+
+    const registerHotkeys = (config: any) => {
+      hotkeysManager.registerHotkeys(config);
+    };
+
+    const heightAtLine = (lineIndex: number) => {
+      const el = textareaEl.value?.$el.querySelector(`section[data-line="${lineIndex}"]`);
 
       return el ? el.offsetTop + el.offsetHeight : 0;
-    },
-    clearTimeout() {
-      if (this.timmer) clearTimeout(this.timmer);
+    };
 
-      this.timmer = null;
-    },
-    updateCurrentHistoryRange() {
-      if (!this.timmer) {
-        this.updateHistory(this.historyIndex, {
-          range: this.getRange(),
+    const clearTheTimeout = () => {
+      if (timer.value) clearTimeout(timer.value);
+      timer.value = undefined;
+    };
+
+    const updateCurrentHistoryRange = () => {
+      if (!timer.value) {
+        updateHistory(historyIndex, {
+          range: getRange(),
         });
       }
-    },
-    handleInput(e) {
-      if (this.isComposing) return;
+    };
 
-      this.$emit('update:modelValue', e.target.value);
-    },
-    saveHistory() {
-      const range = this.getRange();
+    const handleInput = (e: Event) => {
+      if (isComposing.value) return;
+
+      emit('update:modelValue', (<HTMLInputElement>e.target)?.value);
+    };
+
+    type History = {
+      value: string | undefined;
+      range: {
+        start: any;
+        end: any;
+      };
+    };
+    let historyStack: Array<History> = [];
+    let historyIndex = 0;
+    const { historyMax } = toRefs(props);
+
+    const saveHistory = () => {
+      const range = getRange();
       const history = {
-        value: this.modelValue,
+        value: modelValue.value,
         range,
       };
 
-      this.historyStack = this.historyStack.slice(0, this.historyIndex + 1);
-      this.historyStack.push(history);
-      if (this.historyStack.length > this.historyMax) this.historyStack.shift();
-      this.historyIndex = this.historyStack.length - 1;
-    },
-    updateHistory(index, data) {
-      const history = this.historyStack[index];
+      historyStack = historyStack.slice(0, historyIndex + 1);
+      historyStack.push(history);
+      if (historyStack.length > historyMax.value) historyStack.shift();
+      historyIndex = historyStack.length - 1;
+    };
+
+    const updateHistory = (index: number, data: any) => {
+      const history = historyStack[index];
 
       if ('value' in data) history.value = data.value;
       Object.assign(history.range, data.range);
-    },
-    goHistory(index) {
-      const { value, range } = this.historyStack[index];
+    };
 
-      this.$emit('update:modelValue', value);
-      this.triggerInputBySetHistory = true;
+    const goHistory = (index: number) => {
+      const { value, range } = historyStack[index];
 
-      this.$nextTick(() => {
-        this.triggerInputBySetHistory = false;
-        this.setRange(range);
+      emit('update:modelValue', value);
+      triggerInputBySetHistory = true;
+
+      nextTick(() => {
+        triggerInputBySetHistory = false;
+        setRange(range);
       });
-    },
-    getRange() {
+    };
+
+    const getRange = () => {
       return {
-        start: this.textareaEl.selectionStart,
-        end: this.textareaEl.selectionEnd,
+        start: textareaEl.value?.$el.selectionStart,
+        end: textareaEl.value?.$el.selectionEnd,
       };
-    },
-    setRange({ start, end }) {
-      this.textareaEl.setSelectionRange(start, end);
-      this.updateCurrentHistoryRange();
-    },
-    focus() {
-      this.textareaEl.focus();
-    },
-    insertText(text) {
-      insertTextAtCursor(this.textareaEl, text);
-    },
-    undo() {
-      if (this.historyIndex > 0) {
-        this.historyIndex--;
-        this.goHistory(this.historyIndex);
+    };
+
+    const setRange = ({ start, end }: { start: number; end: number }) => {
+      textareaEl.value?.$el.setSelectionRange(start, end);
+      updateCurrentHistoryRange();
+    };
+
+    const focus = () => {
+      textareaEl.value?.$el.focus();
+    };
+
+    const insertText = (text: string) => {
+      insertTextAtCursor(textareaEl.value?.$el, text);
+    };
+
+    const undo = () => {
+      if (historyIndex > 0) {
+        historyIndex--;
+        goHistory(historyIndex);
       }
-    },
-    redo() {
-      if (this.historyIndex < this.historyStack.length - 1) {
-        this.historyIndex++;
-        this.goHistory(this.historyIndex);
+    };
+
+    const redo = () => {
+      if (historyIndex < historyStack.length - 1) {
+        historyIndex++;
+        goHistory(historyIndex);
       }
-    },
+    };
+
+    return {
+      isComposing,
+      textareaEl,
+      handleCompositionStart,
+      handleCompositionUpdate,
+      handleCompositionEnd,
+      handleInput,
+      updateCurrentHistoryRange,
+      handlePaste,
+      handleBlur,
+      undo,
+      redo,
+    };
   },
 });
 </script>
