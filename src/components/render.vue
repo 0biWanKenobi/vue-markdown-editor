8849c36a48f3b4dc7756c77295316c532e577dee
0biWanKenobi <metal.rulesall@gmail.com>
2022-01-07 16:30:36 +0100

    refactor: convert `render` to Vue component

---
+++ src/components/render.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import VueTypes, { func } from 'vue-types';
+
+export default defineComponent({
+  props: {
+    renderFn: VueTypes.instanceOf(Function).isRequired,
+  },
+  setup(props, { attrs }) {
+    return () => props.renderFn(attrs);
+  },
+});
+</script>
