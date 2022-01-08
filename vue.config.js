2e58433ca190227e1505b3b9d1c149e703ad9b7a
0biWanKenobi <metal.rulesall@gmail.com>
2022-01-08 17:56:04 +0100

    add vue config

---
+++ vue.config.js
@@ -0,0 +1,84 @@
+var path = require('path');
+
+const backendLoader = 'style-loader';
+
+const cssExtractPlugin = [];
+
+if (process.env.STYLUS_COV) {
+  console.log('dangit');
+} else {
+  console.log('hmm');
+}
+
+// if (useCssExtract) {
+//   cssExtractPlugin.push(
+//     new MiniCssExtractPlugin({
+//       filename: 'style/[name].css',
+//     })
+//   );
+// }
+
+var nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      extensions: ['.js', '.ts', '.vue', '.css'],
+      alias: {
+        '@': path.join(__dirname, './src'),
+      },
+    },
+    externals: [
+      {
+        consolidate: 'commonjs consolidate',
+      },
+      // nodeExternals(),
+    ],
+    // module: {
+    //   rules: [
+    //     {
+    //       test: /\.tsx?$/,
+    //       loader: 'ts-loader',
+    //       options: {
+    //         appendTsSuffixTo: [/\.vue$/],
+    //       },
+    //       exclude: /node_modules/,
+    //     },
+    //     {
+    //       test: /\.css/,
+    //       sideEffects: true,
+    //       use: [backendLoader, 'css-loader', 'postcss-loader'],
+    //     },
+    //     {
+    //       test: /\.scss/,
+    //       sideEffects: true,
+    //       use: [backendLoader, 'css-loader', 'postcss-loader', 'sass-loader'],
+    //     },
+    //     {
+    //       test: /\.vue$/,
+    //       loader: 'vue-loader',
+    //     },
+    //     // {
+    //     //   test: /\.(js)$/,
+    //     //   exclude: /node_modules/,
+    //     //   use: {
+    //     //     loader: 'babel-loader',
+    //     //     // enable sub-packages to find babel config
+    //     //     options: {
+    //     //       rootMode: 'upward',
+    //     //     },
+    //     //   },
+    //     // },
+    //     {
+    //       test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+    //       loader: 'url-loader',
+    //       options: {
+    //         limit: 10000,
+    //         publicPath: '../',
+    //         name: 'fonts/[name].[hash:7].[ext]',
+    //       },
+    //     },
+    //   ],
+    // },
+  },
+};
