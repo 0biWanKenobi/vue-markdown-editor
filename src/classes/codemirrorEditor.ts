0d3757ccea1dfcea4e94cdc1250c6d4f114b8a6c
0biWanKenobi <metal.rulesall@gmail.com>
2022-01-08 22:21:10 +0100

    feat: implement IEditor interface

--- src/classes/codemirrorEditor.ts
+++ src/classes/codemirrorEditor.ts
@@ -1,23 +1,164 @@
-import IEditor from '@/interfaces/IEditor';
-import { Ref } from 'vue';
+import { ref, Ref } from 'vue';
+import IEditor, { Option, Install } from '@/interfaces/IEditor';
+import { HotKey } from '@/types/hotKeyType';
+import { smooth } from '@/utils/smooth-scroll';
+import useVModel from '@/modules/useVModel';
+import Hotkeys from '@/utils/hotkeys';
 
 class CodemirrorEditor implements IEditor {
   editorEngineEl: Ref<any>;
+  editorScrollerEl: Ref<any>;
   previewScrollerEl: Ref<any>;
   previewEl: Ref<any>;
-  editorFocusEnd: () => void;
-  getCursorLineLeftText: () => string;
-  editorRegisterHotkeys: (...arg: any[]) => void;
-  editorScrollToTop: (scrollTop: number) => void;
-  getScrollInfo: () => { clientHeight: number; top: number; height: number };
-  heightAtLine: (...arg: any[]) => number;
-  focus: () => void;
-  undo: () => void;
-  redo: () => void;
-  clear: () => void;
-  replaceSelectionText: (text: string, replacement?: string) => void;
-  getCurrentSelectedStr: () => string;
-  changeSelectionTo: (insertText: string, selectedText: string) => void;
+  codemirrorInstance: Ref<any>;
+
+  hotkeysManager: Hotkeys;
+
+  constructor() {
+    this.editorEngineEl = ref();
+    this.editorScrollerEl = ref();
+    this.previewScrollerEl = ref();
+    this.previewEl = ref();
+
+    this.codemirrorInstance = ref();
+
+    this.hotkeysManager = new Hotkeys();
+  }
+
+  editorFocusEnd = () => {
+    focus();
+
+    const lastLineIndex = this.codemirrorInstance.value.lastLine();
+    const lastLineContent = this.codemirrorInstance.value.getLine(lastLineIndex);
+
+    this.codemirrorInstance.value.setCursor({ line: lastLineIndex, ch: lastLineContent.length });
+  };
+
+  getCursorLineLeftText = () => {
+    const { line: startLine, ch: startCh } = this.codemirrorInstance.value.getCursor('from');
+    const { line: endLine, ch: endCh } = this.codemirrorInstance.value.getCursor('to');
+
+    if (startLine !== endLine || startCh !== endCh) return;
+
+    return this.codemirrorInstance.value.getLine(startLine).slice(0, startCh);
+  };
+
+  editorRegisterHotkeys = (arg: HotKey) => {
+    this.hotkeysManager.registerHotkeys(arg);
+  };
+
+  editorScrollToTop = (scrollTop: number) => {
+    const currentScrollTop = this.getScrollInfo().top;
+
+    smooth({
+      currentScrollTop,
+      scrollToTop: scrollTop,
+      scrollFn: (scrollTop) => this.codemirrorInstance.value.scrollTo(0, scrollTop),
+    });
+  };
+
+  getScrollInfo = () => {
+    return this.codemirrorInstance.value.getScrollInfo();
+  };
+
+  heightAtLine = (...arg: any) => {
+    return this.codemirrorInstance.value.heightAtLine(...arg);
+  };
+
+  focus = () => {
+    this.codemirrorInstance.value.focus();
+  };
+  // Must implement
+  undo = () => {
+    this.codemirrorInstance.value.undo();
+  };
+  // Must implement
+  redo = () => {
+    this.codemirrorInstance.value.redo();
+  };
+  // Must implement
+  clear = () => {
+    this.codemirrorInstance.value.setValue('');
+  };
+
+  replaceSelectionText = (text: string, type = 'around') => {
+    this.codemirrorInstance.value.replaceSelection(text, type);
+  };
+
+  getCurrentSelectedStr = () => {
+    return this.codemirrorInstance.value.getSelection();
+  };
+
+  getIndexInInterval = (number: number, start: number, end: number) => {
+    if (start <= number && number <= end) {
+      return number - start;
+    }
+  };
+
+  // Must implement
+  changeSelectionTo = (selectedText: string) => {
+    const curStartLine = this.codemirrorInstance.value.getCursor('from');
+    const curEndLine = this.codemirrorInstance.value.getCursor('to');
+
+    if (!selectedText) {
+      this.codemirrorInstance.value.setSelection(curEndLine);
+      return;
+    }
+    const { text } = useVModel();
+    const lines = text.value?.split('\n').slice(curStartLine.line, curEndLine.line + 1);
+    const startIndex = lines && lines.join('\n').indexOf(selectedText, curStartLine.ch);
+    const endIndex = startIndex && startIndex + selectedText.length;
+
+    type Position = {
+      line: number;
+      ch: number | null;
+      indexOfSelected: number | undefined;
+    };
+
+    const start: Position = {
+      line: 0,
+      ch: null,
+      indexOfSelected: startIndex,
+    };
+    const end: Position = {
+      line: 0,
+      ch: null,
+      indexOfSelected: endIndex,
+    };
+
+    let strLen = 0;
+    lines &&
+      lines.find((rowText, lineIndex) => {
+        const rowLength = rowText.length;
+
+        [start, end].forEach((position) => {
+          const newCursor =
+            position.indexOfSelected &&
+            this.getIndexInInterval(position.indexOfSelected, strLen, strLen + rowLength);
+
+          if (typeof newCursor !== 'undefined' && position.ch) {
+            position.ch = newCursor;
+            position.line = curStartLine.line + lineIndex;
+          }
+        });
+
+        // + 1 是算上换行符占的长度
+        strLen += rowLength + 1;
+
+        return start.ch !== null && end.ch !== null;
+      });
+
+    this.codemirrorInstance.value.setSelection(end, start);
+  };
+
+  use(optionsOrInstall: Option | Install, opt?: any) {
+    if (typeof optionsOrInstall === 'function') {
+      optionsOrInstall(opt);
+    } else {
+      (<Install>optionsOrInstall).install(opt);
+    }
+    return this;
+  }
 }
 
 export default CodemirrorEditor;
