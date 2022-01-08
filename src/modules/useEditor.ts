e22b5229c8d945bf6d1cc565c0da27a235f59a91
0biWanKenobi <metal.rulesall@gmail.com>
2022-01-08 22:29:56 +0100

    refactor: useEditor composable
     - add lifecycle hooks manager
     - add emit utility

--- src/modules/useEditor.ts
+++ src/modules/useEditor.ts
@@ -1,8 +1,10 @@
 import IEditor from '@/interfaces/IEditor';
-import { ref } from 'vue';
+import { ref, SetupContext } from 'vue';
 import EDITOR_TYPE, { BASE_EDITOR, CODEMIRROR_EDITOR } from '@/types/editorType';
 import BaseEditor from '@/classes/baseEditor';
 import CodemirrorEditor from '@/classes/codemirrorEditor';
+import { editorEmits } from './common';
+import LifecycleStage from '@/types/lifecycleStage';
 
 const DEFAULT_EDITOR = BASE_EDITOR;
 
@@ -11,17 +13,49 @@ const editorMap: Record<string, () => IEditor> = {
   [CODEMIRROR_EDITOR]: () => new CodemirrorEditor(),
 };
 
+const lifeCycleHooks: Record<string, Function[]> = {
+  [LifecycleStage.mounted]: [],
+  [LifecycleStage.unmounted]: [],
+  [LifecycleStage.beforeMount]: [],
+  [LifecycleStage.beforeUnmount]: [],
+};
+
 const currentEditor = ref<IEditor>();
+const ctx = ref<SetupContext<string[]>>();
 
-export default (editorType: EDITOR_TYPE = DEFAULT_EDITOR) => {
-  if (currentEditor.value)
-    return {
-      editor: currentEditor.value,
-    };
+const setContext = (_ctx: SetupContext<string[]>) => {
+  ctx.value = _ctx;
+};
 
-  currentEditor.value = editorMap[editorType]();
+const setLifeCycleHooks = (stage: LifecycleStage, ...hooks: Function[]) => {
+  lifeCycleHooks[stage].push(...hooks);
+};
+
+const callLifeCycleHooks = (stage: LifecycleStage) => {
+  for (const hook of lifeCycleHooks[stage]) {
+    hook();
+  }
+};
+
+const installEmits = (...ee: string[]) => {
+  editorEmits.push(...ee);
+};
+
+const emit = (e: string, ...args: any[]) => {
+  ctx.value?.emit(e, args);
+};
+
+const useEditor = <T extends IEditor>(editorType: EDITOR_TYPE = DEFAULT_EDITOR) => {
+  if (!currentEditor.value) currentEditor.value = editorMap[editorType]();
 
   return {
-    editor: currentEditor.value,
+    editor: currentEditor.value as T,
+    setLifeCycleHooks,
+    callLifeCycleHooks,
+    installEmits,
+    setContext,
+    emit,
   };
 };
+
+export default useEditor;
