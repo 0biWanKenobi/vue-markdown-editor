f2f5321916a97a39734a8e630f750a370834aa87
0biWanKenobi <metal.rulesall@gmail.com>
2022-01-09 15:12:18 +0100

    feat: use VueTypes

--- src/modules/toolbar.ts
+++ src/modules/toolbar.ts
@@ -1,21 +1,12 @@
+import VueTypes, { array } from 'vue-types';
+
 export const toolbarProps = {
-  leftToolbar: {
-    type: String,
-    default:
-      'undo redo clear | h bold italic strikethrough quote | ul ol table hr | link image code | save',
-  },
-  rightToolbar: {
-    type: String,
-    default: 'preview toc sync-scroll fullscreen',
-  },
-  toolbar: {
-    type: Object,
-    default: () => ({}),
-  },
-  disabledMenus: {
-    type: Array,
-    default: () => ['image/upload-image'],
-  },
+  leftToolbar: VueTypes.string.def(
+    'undo redo clear | h bold italic strikethrough quote | ul ol table hr | link image code | save'
+  ),
+  rightToolbar: VueTypes.string.def('preview toc sync-scroll fullscreen'),
+  toolbar: VueTypes.object.def({}),
+  disabledMenus: array<string>().def(['image/upload-image']),
   toolbarConfig: {
     type: Object,
     default: () => ({}),
