20a8f95e93a385ab3a8c067fddee4b9476209175
0biWanKenobi <metal.rulesall@gmail.com>
2022-01-09 15:06:04 +0100

    fix: cdn Mermaid plugin typings

--- src/plugins/mermaid/cdn.ts
+++ src/plugins/mermaid/cdn.ts
@@ -1,8 +1,9 @@
 import creator from './creator';
+import type { Mermaid } from 'mermaid';
 
 const isServer = typeof window === 'undefined';
 if (!isServer && !window.mermaid) {
   console.error('Please import resources mermaid from cdn');
 }
 
-export default creator(!isServer ? window.mermaid : null);
+export default creator(!isServer ? (window.mermaid as unknown as Mermaid) : undefined);
