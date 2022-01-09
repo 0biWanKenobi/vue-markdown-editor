d46fd2039e1f897df6b748a33f941bdb469763b4
0biWanKenobi <metal.rulesall@gmail.com>
2022-01-09 15:23:08 +0100

    fix: types

--- src/utils/constants/editor-mode.ts
+++ src/utils/constants/editor-mode.ts
@@ -1,5 +1,9 @@
-export default {
+import EDITOR_MODE_TYPE from '@/types/editorMode';
+
+const EditorMode: Record<string, EDITOR_MODE_TYPE> = {
   PREVIEW: 'preview',
   EDITABLE: 'editable',
   EDIT: 'edit',
 };
+
+export default EditorMode;
