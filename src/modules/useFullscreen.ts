0debfa904a6e325a45142212d3512375b435f78f
0biWanKenobi <metal.rulesall@gmail.com>
2022-01-09 15:13:46 +0100

    refactor: use lifecycle stages

--- src/modules/useFullscreen.ts
+++ src/modules/useFullscreen.ts
@@ -1,4 +1,6 @@
-import { ref, watch } from 'vue';
+import LifecycleStage from '@/types/lifecycleStage';
+import { ref, SetupContext, watch } from 'vue';
+import useEditor from './useEditor';
 
 const fullscreen = ref(false);
 
@@ -11,17 +13,10 @@ const handleWindowKeyup = (e: KeyboardEvent) => {
 const toggleFullScreen = (value = !fullscreen.value) => {
   fullscreen.value = value;
   const { 0: html, 1: body }: Record<number, HTMLElement> = document.querySelectorAll('html, body');
-  const overflow = fullscreen.value ? 'hidden' : null;
+  const overflow = fullscreen.value ? 'hidden' : 'visible';
 
   body.style.overflow = overflow;
   html.style.overflow = overflow;
-};
-
-const addWatches = (emit: (e: string, ...args: any[]) => void) => {
-  watch(
-    () => fullscreen.value,
-    () => emit('fullscreen-change', fullscreen.value)
-  );
 };
 
 const onMounted = () => {
@@ -36,14 +31,24 @@ const onBeforeUnmount = () => {
   window.removeEventListener('keyup', handleWindowKeyup, false);
 };
 
-export default (defaults?: { fullscreen: boolean }) => {
+const { setLifeCycleHooks } = useEditor();
+setLifeCycleHooks(LifecycleStage.mounted, onMounted);
+setLifeCycleHooks(LifecycleStage.beforeUnmount, onBeforeUnmount);
+
+export default (
+  ctx?: SetupContext<string[]> | SetupContext<Record<string, any>>,
+  defaults?: { fullscreen: boolean }
+) => {
   if (defaults) fullscreen.value = defaults.fullscreen;
+
+  watch(
+    () => fullscreen.value,
+    () => ctx?.emit('fullscreen-change', fullscreen.value)
+  );
+
   return {
     fullscreen,
     handleWindowKeyup,
     toggleFullScreen,
-    addWatches,
-    onMounted,
-    onBeforeUnmount,
   };
 };
