ef0f418097e7972bc7253fec55c8c9b1c0961af0
0biWanKenobi <metal.rulesall@gmail.com>
2022-01-09 21:09:52 +0100

    refactor: use VueTypes

--- src/modules/preview.ts
+++ src/modules/preview.ts
@@ -1,16 +1,9 @@
+import VueTypes, { object } from 'vue-types';
+
 export const previewProps = {
-  tabSize: {
-    type: Number,
-    default: 2,
-  },
-  scrollContainer: {
-    type: Function,
-    default: () => window,
-  },
-  top: {
-    type: Number,
-    default: 0,
-  },
+  tabSize: VueTypes.number.def(2),
+  scrollContainer: VueTypes.oneOfType([Element, Window]).def(window),
+  top: VueTypes.number.def(0),
 };
 
 export const previewEmits = ['image-click'];
