a602a114cc35cfe4b8b6959628d2cf46c916a3a6
0biWanKenobi <metal.rulesall@gmail.com>
2022-01-09 15:07:52 +0100

    fix: mermaid plugin
     - get preview from usePreview composable
     - fix null checks
     - implement initialization

--- src/plugins/mermaid/creator.ts
+++ src/plugins/mermaid/creator.ts
@@ -5,25 +5,23 @@ import PluginCreatorFn from '@/types/pluginCreatorFn';
 import useVMdParser from '@/modules/useVMdParser';
 import { Mermaid } from 'mermaid';
 import { nextTick, watch } from 'vue';
-import useEditor from '@/modules/useEditor';
+import usePreview from '@/modules/usePreview';
 
-export default function creator(mermaid: Mermaid) {
+export default function creator(mermaid: Mermaid | undefined) {
   async function handleMdChange() {
     if (!inBrowser) return;
 
     await nextTick();
 
-    const {
-      editor: { previewEl },
-    } = useEditor();
-    const eles = previewEl.value.querySelectorAll('.v-md-mermaid');
+    const { previewEl } = usePreview();
+    const eles = previewEl.value?.querySelectorAll('.v-md-mermaid');
 
-    if (!eles.length) return;
+    if (!eles?.length) return;
 
     let parseSuccess = false;
     eles.forEach((ele: any) => {
       try {
-        parseSuccess = mermaid.parse(ele.innerText);
+        parseSuccess = mermaid!.parse(ele.innerText);
       } catch (e) {
         const err = e as any;
         if (!err.str) {
@@ -32,7 +30,7 @@ export default function creator(mermaid: Mermaid) {
       }
 
       // if (parseSuccess) mermaid.init(null, ele);
-      if (parseSuccess) mermaid.init(ele);
+      if (parseSuccess) mermaid!.init(ele);
     });
   }
 
@@ -65,23 +63,13 @@ export default function creator(mermaid: Mermaid) {
         const vMdParser = useVMdParser();
         vMdParser.use(parser);
         // was on created
-        mermaid.initialize(initialize);
-
-        //TODO: investigate how to integrate mermaid plugin into "preview" Vue components
-        // const mixin = {
-        //   watch: {
-        //     html: {
-        //       immediate: true,
-        //       handler: handleMdChange,
-        //     },
-        //   },
-        // };
-
-        // if (VMdEditor.name === 'v-md-editor') {
-        //   VMdEditor.Preview.mixins.push(mixin);
-        // } else {
-        //   VMdEditor.mixins.push(mixin);
-        // }
+        mermaid!.initialize(initialize);
+        const { html } = usePreview();
+        watch(
+          () => html.value,
+          (_) => handleMdChange(),
+          { immediate: true }
+        );
       },
     };
   };
