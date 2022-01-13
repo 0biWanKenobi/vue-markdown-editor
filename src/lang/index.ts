938ae5be4c73fb98850d527e6d2cbaae1ab9abba
0biWanKenobi <metal.rulesall@gmail.com>
2022-01-13 17:11:21 +0100

    feat: index exporting all langs

---
+++ src/lang/index.ts
@@ -0,0 +1,9 @@
+export { default as DE } from './de-DE';
+export { default as US } from './en-US';
+export { default as ES } from './es-ES';
+export { default as FR } from './fr-FR';
+export { default as KR } from './ko-KR';
+export { default as PL } from './pl-PL';
+export { default as RU } from './ru-RU';
+export { default as CN } from './zh-CN';
+export { default as TW } from './zh-TW';
