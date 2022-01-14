a355db6ed262ac67c7128252c8cd031daa46a1b9
0biWanKenobi <metal.rulesall@gmail.com>
2022-01-14 10:07:30 +0100

    fix: custom html page only in development

--- vue.config.js
+++ vue.config.js
@@ -2,12 +2,14 @@ var path = require('path');
 
 module.exports = {
   chainWebpack: (config) => {
-    config.plugin('html').tap((args) => {
       if (process.env.NODE_ENV == 'development') {
+      config.plugin('html').tap((args) => {
+        {
         args[0].template = './dev/index.html';
       }
       return args;
     });
+    }
   },
   configureWebpack: {
     target: 'web',
