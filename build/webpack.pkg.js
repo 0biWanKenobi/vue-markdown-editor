bd310a6b921ddd44931272ea06ea86c46db2b3c2
0biWanKenobi <metal.rulesall@gmail.com>
2022-01-15 17:09:07 +0100

    fix: webpack.pkg
    - removed references to entryFiles
    - fixed entry point
    - removed self from globalObject

--- build/webpack.pkg.js
+++ build/webpack.pkg.js
@@ -1,20 +1,18 @@
+// https://5balloons.info/create-publish-you-first-vue-plugin-on-npm-the-right-way/
+// https://www.codemag.com/Article/2103071/The-Complete-Guide-to-Vue-3-Plug-ins-Part-2
+// https://blog.logrocket.com/building-vue-3-component-library/
+// https://dev.to/htech/creating-a-vue-module-with-rollup-and-typescript-4igb
+
 const path = require('path');
 const merge = require('webpack-merge');
 const getBaseConfig = require('./webpack.base');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const cssnano = require('cssnano');
-const { entryFiles } = require('./build-entry');
-
-const entry = {};
-
-entryFiles.forEach((fileName) => {
-  entry[fileName] = `./src/${fileName}.js`;
-});
 
 module.exports = merge(getBaseConfig({ useCssExtract: true }), {
   mode: 'production',
-  entry,
+  entry: path.join(__dirname, '../src/main.ts'),
   output: {
     library: 'VMdEditor',
     libraryTarget: 'umd',
@@ -22,7 +20,6 @@ module.exports = merge(getBaseConfig({ useCssExtract: true }), {
     path: path.join(__dirname, '../lib'),
     umdNamedDefine: true,
     filename: '[name].js',
-    globalObject: "typeof self !== 'undefined' ? self : this",
   },
   externals: {
     vue: {
