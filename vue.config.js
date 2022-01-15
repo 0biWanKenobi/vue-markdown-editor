248056ac2f0cdb0bde652fd9bb010c04da3c328c
0biWanKenobi <metal.rulesall@gmail.com>
2022-01-15 17:14:47 +0100

    feat: build config
    - one for d.ts files
    - one for building, excludes dev folder
    - added outDir to base tsConfig
    - cleaned includes and excludes in base tsConfig
    - configured custom tsConfig in vue config
    - added entrypoints to vue config
      ( only works in non-lib mode)
    - mark vue as external in vue config

--- vue.config.js
+++ vue.config.js
@@ -1,28 +1,59 @@
 var path = require('path');
 
+const TSCONFIG_PATH = path.resolve(__dirname, './app.tsconfig.json');
+
 /**
  *  @typedef { import("@vue/cli-service").ProjectOptions } Options
  *  @type { Options }
  */
 module.exports = {
   outputDir: path.join(__dirname, './lib'),
+  filenameHashing: false,
   chainWebpack: (config) => {
-      if (process.env.NODE_ENV == 'development') {
-      config.plugin('html').tap((args) => {
-        {
-        args[0].template = './dev/index.html';
-      }
+    config.plugin('fork-ts-checker').tap((args) => {
+      args[0].typescript.configFile = './app.tsconfig.json';
       return args;
     });
+
+    config.module
+      .rule('ts')
+      .use('ts-loader')
+      .merge({
+        options: {
+          configFile: TSCONFIG_PATH,
+        },
+      });
+
+    if (process.env.NODE_ENV == 'development') {
+      config.plugin('html').tap((args) => {
+        {
+          args[0].template = './dev/index.html';
+        }
+        return args;
+      });
+    } else {
+      config.optimization.splitChunks(false).minimize(false);
+      // allow to access default export from src/main.ts direcly, w/out typing VMdEditor.default
+
+      // config.output.filename('VMdEditor.[name].js').library('VMdEditor');
+
+      config.output.library(['VMdEditor', '[name]']).libraryTarget('umd').umdNamedDefine(true);
+
+      config.entry('preview').add('./src/preview.ts');
+      config.entry('editor').add('./src/editor.ts');
+      config.entry('lang').add('./src/lang/index.ts');
+      config.entry('types').add('./src/types.ts');
+      config.entry('theme').add('./src/theme/index.ts');
+      config.entry('plugins').add('./src/plugins/index.ts');
     }
   },
   configureWebpack: {
     target: 'web',
     devtool: 'source-map',
     // allow to access default export from src/main.ts direcly, w/out typing VMdEditor.default
-    output: {
-      libraryExport: 'default',
-    },
+    // output: {
+    //   libraryExport: 'default',
+    // },
     resolve: {
       extensions: ['.js', '.ts', '.vue', '.css'],
       alias: {
@@ -32,6 +63,14 @@ module.exports = {
     externals: [
       {
         consolidate: 'commonjs consolidate',
+      },
+      {
+        vue: {
+          root: 'Vue',
+          commonjs: 'vue',
+          commonjs2: 'vue',
+          amd: 'vue',
+        },
       },
     ],
   },
