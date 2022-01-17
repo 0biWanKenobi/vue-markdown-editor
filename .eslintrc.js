b39e3b4d62a2afbc0f5234a7960e6b14e5c9fcb2
0biWanKenobi <metal.rulesall@gmail.com>
2022-01-17 01:51:01 +0100

    fix: restore eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,8 +7,10 @@ module.exports = {
   },
   extends: [
     'plugin:vue/vue3-essential',
-    '@vue/eslint-config-typescript/recommended',
-    '@vue/eslint-config-prettier',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint',
   ],
   parserOptions: {
     ecmaVersion: 2020,
@@ -16,8 +18,5 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-unused-vars': ['error', { args: 'all', argsIgnorePattern: '^_' }],
-    '@typescript-eslint/no-non-null-assertion': 'off',
   },
 };
