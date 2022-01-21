0195ac7779fef1ef2f243973156582b332e9faab
0biWanKenobi <metal.rulesall@gmail.com>
2022-01-21 13:03:46 +0100

    feat: entry point for commonjs build

---
+++ src/main.cjs.ts
@@ -0,0 +1,7 @@
+export { default } from './main.esm';
+export * from './editor';
+export * from './preview';
+export * from './theme';
+export * from './lang';
+export * from './plugins';
+export * from './types';
